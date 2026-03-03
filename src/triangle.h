a96780c8b436c735f807f260f5428e0e952c4fde
John-Gallardo <johnpatrickgallardo33@gmail.com>
2026-03-02 21:40:11 -0700

    Feat: implement class for a triangle

---
+++ src/triangle.h
@@ -0,0 +1,17 @@
+/**
+ * @brief A triangle simply contains the 3 indices of a vertex.
+ */
+class Triangle {
+    public:
+        // Constructor
+        Triangle(int i, int j, int k)
+            : m_indices {i, j, k}
+        {}
+
+        // Getters
+        std::vector<int> getIndices() {
+            return m_indices;
+        }
+    private:
+        std::vector<int> m_indices;
+};
