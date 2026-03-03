f3a4eb08bc6cbc3b8432e21d798810a1add23d57
John-Gallardo <johnpatrickgallardo33@gmail.com>
2026-03-02 21:45:41 -0700

    Feat: Implement face class

---
+++ src/face.h
@@ -0,0 +1,17 @@
+/**
+ * @brief A face simply contains 3 triangles.
+ */
+class Face {
+    public:
+        // Constructor
+        Face(Triangle t1, Triangle t2, Triangle t3)
+            : m_triangles {t1, t2, t3}
+        {}
+
+        // Getter
+        std::vector<Triangle> getTriangles() {
+            return m_triangles;
+        }
+    private:
+        std::vector<Triangle> m_triangles;
+};
